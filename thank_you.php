c2f27db1bc2d01b0dc3e8a00d9cddb86e9e48066
tiago <tiagolemespalhano@gmail.com>
2022-06-07 17:10:10 -0300

    Thank You page part 1

--- thank_you.php
+++ thank_you.php
@@ -1,1 +1,31 @@
-thank_you.php
+<?php 
+
+require_once("resources/config.php");
+include(TEMPLATE_FRONT . DS . "header.php");
+
+// sem transação não mostra a página
+if (!isset($_GET['amt']) || !isset($_GET['cc']) || !isset($_GET['tx']) || !isset($_GET['st'])) {
+	header("Location: .");	
+	die();
+}
+
+$amount = $_GET['amt'];
+$currency = $_GET['cc'];
+$transaction = $_GET['tx'];
+$status = $_GET['st'];
+
+?>
+
+
+
+
+
+
+<div class="container">
+
+	<h1 class="text-center alert alert-success">Thank You</h1>
+
+
+<hr>
+
+<?php include(TEMPLATE_FRONT . DS . "footer.php") ?>
